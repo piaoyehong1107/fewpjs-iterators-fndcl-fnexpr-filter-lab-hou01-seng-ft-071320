802ed73fbcc18fdb6ccd20c294050d2e05ed982b
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-05 22:38:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(arr,str){
+  let newarr=arr.filter(dri=>{
+   return dri.toLowerCase()===str.toLowerCase()
+  })
+  return newarr
+}
+function fuzzyMatch(arr,str){
+  let newarr=arr.filter(dri=>{
+   return dri.startsWith(str)
+  })
+  return newarr
+}
+
+function matchName(arr,str){
+  let newarr=arr.filter(dri=>{
+   return dri.name===str
+  })
+  return newarr
+}
