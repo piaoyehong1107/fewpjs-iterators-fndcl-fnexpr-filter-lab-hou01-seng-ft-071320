7f263724b9010f4c6e83aadead34ad4014d296fc
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-05 22:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findMatching(arr,str){
   let newarr=arr.filter(dri=>{
-   return dri.toLowerCase===str.toLowerCase
+   return dri.toLowerCase()===str.toLowerCase()
   })
   return newarr
 }
